efcf7e1b75400ade679b9a0815600867afe1b76d
pjw <pjw@DESKTOP-HKR7D8L>
2023-09-25 10:14:59 +0900

    제품입력 일단 구현 완료.

--- mit/src/main/java/edu/mit/controller/ProductController.java
+++ mit/src/main/java/edu/mit/controller/ProductController.java
@@ -3,8 +3,10 @@ package edu.mit.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import edu.mit.domain.ProductVO;
 import edu.mit.service.ProductService;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j;
@@ -24,5 +26,11 @@ public class ProductController {
 	@GetMapping("/input")
 	public void input(Model m) {
 		m.addAttribute("groupcode", service.readGroupCode());
+	}
+	
+	@PostMapping("/create")
+	public String create(ProductVO vo) {
+		service.create(vo);
+		return "redirect:/product/mainMenu";
 	}
 }
--- mit/src/main/webapp/WEB-INF/views/product/input.jsp
+++ mit/src/main/webapp/WEB-INF/views/product/input.jsp
@@ -17,7 +17,7 @@
 				<li>제품원가&nbsp;<input type="text" name="cost"></li>
 				<li>목표수량&nbsp;<input type="text" name="pnum"></li>
 				<li>재고수량&nbsp;<input type="text" name="jnum"></li>
-				<li>출고가&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="sale"></li>
+				<li>출고가&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="sale"></li>
 				<li>
 					그룹이름
 					<select name="gcode">
